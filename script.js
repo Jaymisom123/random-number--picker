48ed5c2b69d5c6a6cc5940b72ade2088e88bc796
Jaymisom123 <jaymerego2015@gmail.com>
2025-02-18 17:16:34 -0300

    mudando código

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+      if(max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    const drawNumber = document.querySelector(".result-number").innerHTML = result
-
-    drawNumber = result
+        const drawNumber = document.querySelector(".result-number").innerHTML = result
     
-
+        drawNumber = result
+    
+      } else{
+        alert("the first number must be greater than the second")
+      }
+    
+       
    
-
 };
